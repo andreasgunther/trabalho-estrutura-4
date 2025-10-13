6a516c12b77c590b336da029198fad0b4c68975e
Davi Carvalho <168906459+davicarvalh0@users.noreply.github.com>
2025-10-13 10:57:55 -0300

    Update main.c

---
+++ lista_dupla/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+int main(){
+    struct Musica *playlist = NULL; //ponteiro para início da playlist
+    int n;
+    do {
+        system("clear");
+        printf("=============== MENU ===============\n");
+        printf("[0] - Sair\n");
+        printf("[1] - Inserir música no início\n");
+        printf("[2] - Inserir música no final\n");
+        printf("[3] - Remover primeira música\n");
+        printf("[4] - Remover ultima música\n");
+        printf("[5] - Buscar música\n");
+        printf("[6] - Imprimir playlist\n");
+        printf("====================================\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &n);
+        switch(n){
+            case 0:
+                printf("Código finalizado!\n");
+                break;
+            
+            case 1:
+                inserir_inicio(&playlist);
+                break;
+
+            case 2:
+                inserir_final(&playlist);
+                break;
+
+            case 3:
+                remover_inicio(&playlist);
+                break;
+
+            case 4:
+                remover_final(&playlist);
+                break;
+
+            case 5:
+                buscar(&playlist);
+                break;
+
+            case 6:
+                imprimir(playlist);
+                break;
+
+            default:
+                printf("Valor Inválido!\n");
+                printf("Pressione Enter para continuar\n");
+                getchar(); getchar();
+                break;
+        }
+    } while(n != 0);
+    return 0;
+}
