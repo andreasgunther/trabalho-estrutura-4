16533e07ba01f32cce201e6795665dd1f658c403
Davi Carvalho <168906459+davicarvalh0@users.noreply.github.com>
2025-10-16 09:56:27 -0300

    Update lista.h

--- lista_dupla/lista.h
+++ lista_dupla/lista.h
@@ -11,10 +11,11 @@ struct Musica {
 void inserir_inicio(struct Musica **playlist);
 void inserir_final(struct Musica **playlist);
 void remover_inicio(struct Musica **playlist);
+void remover_atual(struct Musica **atual, struct Musica **playlist);
 void remover_final(struct Musica **playlist);
 struct Musica* buscar(struct Musica **playlist); //tipo struct porque retona um ponteiro para struct
 void passar_musica(struct Musica **atual);
-void musica_atual(struct Musica **atual);
+void musica_atual(struct Musica **atual, struct Musica **playlist);
 void voltar_musica(struct Musica **atual);
 void imprimir(struct Musica *playlist);
 
